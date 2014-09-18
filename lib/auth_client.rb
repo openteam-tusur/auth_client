42a3a5e05cc97c765dec60e7fceefb5eb966a6e9
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-09-18 15:06:25 +0700

    User class

--- lib/auth_client.rb
+++ lib/auth_client.rb
@@ -1,4 +1,6 @@
 require "auth_client/version"
 
+require 'auth_client/user'
+
 module AuthClient
 end
---
+++ lib/auth_client/user.rb
@@ -0,0 +1,21 @@
+require 'auth_redis_user_connector'
+
+class User
+  def self.find_by(id: nil)
+    return nil unless id
+
+    RedisUserConnector.get(id).merge('id' => id)
+  end
+
+  def initialize(options)
+    options.each_pair do |key, val|
+      self.class_eval("def #{key}; @#{key}; end")
+      self.instance_variable_set "@#{key}", val
+    end
+  end
+
+  def to_s
+    [surname, name, patronymic].compact.join(' ')
+  end
+end
+
