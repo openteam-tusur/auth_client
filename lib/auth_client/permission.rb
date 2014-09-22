6b0881ed3392a7a8c242782a56fe023c596455e9
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-09-22 17:13:17 +0700

    stuff for permission

--- lib/auth_client/permission.rb
+++ lib/auth_client/permission.rb
@@ -7,8 +7,16 @@ module AuthClient
     module ClassMethods
       def acts_as_auth_client_permission(roles: roles)
         define_singleton_method :available_roles do
-          roles
+          roles.map(&:to_s)
         end
+
+        belongs_to :context, :polymorphic => true
+
+        scope :for_role, ->(role) { where(:role => role)  }
+        scope :for_context, ->(context) { where(:context_id => context.try(:id), :context_type => context.try(:class))  }
+
+        validates_inclusion_of :role, :in => available_roles + available_roles.map(&:to_sym)
+        validates_presence_of :role
       end
     end
   end
